a83fc3417e4b9807d86cf305d3a792ebba59ee4c
HenryZ5734 <200110614@stu.hit.edu.cn>
2022-05-13 10:05:31 +0800

    增加了初始化屏幕参数

--- app/src/main/java/com/hitsz/aircraftwar/application/MainActivity.java
+++ app/src/main/java/com/hitsz/aircraftwar/application/MainActivity.java
@@ -1,14 +1,26 @@
-package com.hitsz.aircraftwar;
+package com.hitsz.aircraftwar.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.hitsz.aircraftwar.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int WINDOW_HEIGHT;
+    public static int WINDOW_WIDTH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 初始化屏幕参数
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        WINDOW_HEIGHT = dm.heightPixels;
+        WINDOW_WIDTH =dm.widthPixels;
     }
 }
