5de838bd66a04fa4b4995f4768c310dd9e3aaf08
HenryZ5734 <200110614@stu.hit.edu.cn>
2022-05-20 17:23:03 +0800

    完善开始界面

--- app/src/main/java/com/hitsz/aircraftwar/application/MainActivity.java
+++ app/src/main/java/com/hitsz/aircraftwar/application/MainActivity.java
@@ -1,12 +1,23 @@
 package com.hitsz.aircraftwar.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
 import com.hitsz.aircraftwar.R;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnEasy;
+    private Button btnNormal;
+    private Button btnHard;
+    private SwitchCompat swMusic;
+
+    private GameView game;
+    private String difficulty;
 
     public static int WINDOW_HEIGHT;
     public static int WINDOW_WIDTH;
@@ -22,8 +33,30 @@ public class MainActivity extends AppCompatActivity {
         WINDOW_HEIGHT = dm.heightPixels;
         WINDOW_WIDTH =dm.widthPixels;
 
+        // 初始化图片资源
         ImageManager.initial(getResources());
-        GameView game=new EasyGame(this);
-        setContentView(game);
+
+        // 进入开始界面
+        btnEasy = findViewById(R.id.easy);
+        btnNormal = findViewById(R.id.normal);
+        btnHard = findViewById(R.id.hard);
+        swMusic = findViewById(R.id.music);
+
+        // 进入游戏界面
+        btnEasy.setOnClickListener(v -> {
+            difficulty = "EASY";
+            game = new EasyGame(this);
+            setContentView(game);
+        });
+        btnNormal.setOnClickListener(v -> {
+            difficulty = "NORMAL";
+            game = new NormalGame(this);
+            setContentView(game);
+        });
+        btnHard.setOnClickListener(v -> {
+            difficulty = "HARD";
+            game = new HardGame(this);
+            setContentView(game);
+        });
     }
 }
